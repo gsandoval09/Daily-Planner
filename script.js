e3c59a5277ecd890b191b043e87d2cc613fffbf0
George Sandoval <gssandoval09@gmail.com>
2021-10-01 00:56:47 -0500

    added extra color to the background

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.getElementById("currentDay").textContent= moment().format("MM Do YYYY")
 var present= moment().format("H")
 today.format("H")
 
+
+
 // if (present < 9){
 //   document.getElementByID () time-block, add future 
 // } else if (9==present){
