90763fd2534cd3d5285ba0a3bface121aef290f3
George Sandoval <gssandoval09@gmail.com>
2021-10-01 00:09:00 -0500

    added functioning textarea boxes

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ document.getElementById("currentDay").textContent= moment().format("MM Do YYYY")
 var present= moment().format("H")
 today.format("H")
 
-if (present < 9){
-  document.getElementByID () time-block, add future 
-} else if (9==present){
-  9=present; time-block; add present
-} else if(9 < present){
-  9= past; time-block; add past
-} 
+// if (present < 9){
+//   document.getElementByID () time-block, add future 
+// } else if (9==present){
+//   9=present; time-block; add present
+// } else if(9 < present){
+//   9= past; time-block; add past
+// } 
+
+
+
